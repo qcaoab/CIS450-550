9dc62bc9347848366227b3693e2753e55ade4fc0
Qinglan Cao <qcaoab@seas.upenn.edu>
2020-05-02 16:55:54 -0600

    update queries

--- server/index.js
+++ server/index.js
@@ -18,6 +18,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // routes.getAllGenres, specified in routes.js.
 app.get("/randomBooks/:num", routes.randomBooks);
 app.get("/popularBooks", routes.popularBooks);
+app.get("/bestReviews", routes.bestReviews);
+app.get("/controBooks", routes.bestBooks);
+app.get("/onehit", routes.onehit);
+app.get("/prolificAuthors",routes.prolificAuthors);
+app.get("/mostGenreAuthors", routes.mostGenreAuthors);
+app.get("/multiGenreAuthor", routes.multiGenreAuthors);
+app.get("/similarTitles/:str", routes.smimilarTitles)
+app.get("/getAuthor/:author_id", routes.getAuthor);
+app.get("/getReview/:book_id", routes.getReview);
+app.get("/getBooksByAuthor/:author_id", routes.getBooksByAuthor);
+app.get("/getGenresByAuthor/:author_id", routes.getGenresByAuthor);
+
 
 app.listen(8081, () => {
   console.log(`Server listening on PORT 8081`);
